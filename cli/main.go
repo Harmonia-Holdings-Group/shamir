713fe50c43e706bb2433d10764d0e7d5cf75f6a5
Diego Jardon <diegojardonc@gmail.com>
2021-01-19 20:34:16 -0600

    Add: pseudo-test driven by maing

--- cli/main.go
+++ cli/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/pablotrinidad/shamir/crypto"
 )
 
 func main() {
 	fmt.Println("Hola")
 	fmt.Println(crypto.P)
+
+	var key [32]byte
+	keyShares, err := crypto.GenKeyShares(key, 2, 3)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(keyShares)
 }
